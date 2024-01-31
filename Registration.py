84fcc236125b2ddde3837d6ced608cb218fd9f3c
Zafran <zraaeae@gmail.com>
2024-01-30 21:28:37 -0800

    push

--- Registration.py
+++ Registration.py
@@ -1,15 +1,18 @@
-# test_pytest
+# test_pytest.py
 import pytest
 from playwright.sync_api import sync_playwright
-import os
 @pytest.fixture(scope="function")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         yield browser
         browser.close()
-password = os.environ.get('c_pas')
-def test_LoginPass_correctdata(browser):
+def Registration(browser):
     page = browser.new_page()
     page.goto("https://www.demoblaze.com/")
+    title=page.title()
+    assert title =="STORE"
+
+
+
    
