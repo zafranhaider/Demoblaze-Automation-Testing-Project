3a05b8f6357d82dbf677f54460c25f5ea1cb30ba
Zafran <zraaeae@gmail.com>
2024-01-30 20:55:34 -0800

    push

--- README.md
+++
@@ -1,3 +0,0 @@
-# MUST-CMS-Automation-Testing-Projact
-Automating My University CMS To Test It's Functionalities
--My Automation Projact using Playwright
---
+++ Registration.py
@@ -0,0 +1,15 @@
+# test_pytest.py
+import pytest
+from playwright.sync_api import sync_playwright
+import os
+@pytest.fixture(scope="function")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        yield browser
+        browser.close()
+password = os.environ.get('c_pas')
+def test_LoginPass_correctdata(browser):
+    page = browser.new_page()
+    page.goto("https://cms.must.edu.pk:8082/login.aspx")
+   
