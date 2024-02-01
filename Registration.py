45f5694edbd20965a305580947844ddc297f18f6
Zafran <zraaeae@gmail.com>
2024-02-01 14:57:23 -0800

    reg

--- Registration.py
+++ Registration.py
@@ -7,11 +7,23 @@ def browser():
         browser = p.chromium.launch(headless=False)
         yield browser
         browser.close()
-def Registration(browser):
+def test_Registration(browser): 
     page = browser.new_page()
     page.goto("https://www.demoblaze.com/")
     title=page.title()
     assert title =="STORE"
+    page.get_by_role("link", name="Sign up").click()
+    page.get_by_label("Username:").click()
+    page.get_by_label("Username:").fill("dab123")
+    page.get_by_label("Password:").click()
+    page.get_by_label("Password:").fill("123456")
+    page.get_by_label("Password:").click()
+    page.once("dialog", lambda dialog: dialog.dismiss())
+    page.get_by_role("button", name="Sign up").click()
+    title=page.title()
+    assert title == "STORE"
+
+
 
 
 
